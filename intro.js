ab13f3d75fc4c5ccc807fa639b8a9b03dca1810e
Erin Egloff <egloff.erin@gmail.com>
2021-05-10 00:00:06 -0700

    first commit

---
+++ intro.js
@@ -0,0 +1,13 @@
+var olive = {
+    name: 'olive',
+    age: 2,
+    breed: 'siamese',
+    mom: {
+        name: 'erin',
+        age: 23,
+        phone: '8126306419'
+    }
+};
+console.log(olive);
+console.log(olive.mom)
+console.log(olive.mom.name)
